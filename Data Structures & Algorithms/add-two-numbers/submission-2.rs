62d0dacd636e1167b3fdc25542d7a723f3756548
Raghav Vikramprabhu <52612977+DogeDevYT@users.noreply.github.com>
2026-06-06 13:05:58 -0400

    Add: add-two-numbers - submission-2

---
+++ Data Structures & Algorithms/add-two-numbers/submission-2.rs
@@ -0,0 +1,39 @@
+// Definition for singly-linked list.
+// #[derive(PartialEq, Eq, Clone, Debug)]
+// pub struct ListNode {
+//     pub val: i32,
+//     pub next: Option<Box<ListNode>>,
+// }
+//
+// impl ListNode {
+//     #[inline]
+//     pub fn new(val: i32) -> Self {
+//         ListNode { next: None, val }
+//     }
+// }
+
+impl Solution {
+    pub fn add_two_numbers(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        let mut dummy = Box::new(ListNode::new(0));
+        let mut cur = &mut dummy;
+        let mut l1 = l1;
+        let mut l2 = l2;
+        let mut carry = 0;
+
+        while l1.is_some() || l2.is_some() || carry != 0 
+        {
+            let v1 = l1.as_ref().map_or(0, |n| n.val);
+            let v2 = l2.as_ref().map_or(0, |n| n.val);
+
+            let sum = v1 + v2 + carry;
+            carry = sum / 10;
+            cur.next = Some(Box::new(ListNode::new(sum % 10)));
+
+            cur = cur.next.as_mut().unwrap();
+            l1 = l1.and_then(|n| n.next);
+            l2 = l2.and_then(|n| n.next);
+        }
+
+        dummy.next
+    }
+}
