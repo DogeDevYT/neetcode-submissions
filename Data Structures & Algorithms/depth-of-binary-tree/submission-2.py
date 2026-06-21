2e2e813111d7ccb05a9f68705877e1851f5aaec0
Raghav Vikramprabhu <52612977+DogeDevYT@users.noreply.github.com>
2026-06-21 12:35:13 -0400

    Add: depth-of-binary-tree - submission-2

---
+++ Data Structures & Algorithms/depth-of-binary-tree/submission-2.py
@@ -0,0 +1,32 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+"""
+Seems like simple recursive DFS but I'll try iterative with Stack as well
+
+got stack, iterative DFS,
+
+lets get BFS now as well
+"""
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        q = deque()
+
+        if root: q.append(root)
+
+        depth = 0
+        while q:
+            for i in range(len(q)):
+                node = q.popleft()
+
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+                
+            depth += 1
+        return depth
