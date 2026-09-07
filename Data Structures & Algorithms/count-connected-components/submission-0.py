5db0461560aadaec556a54ed88548ea77eec875d
Raghav Vikramprabhu <52612977+DogeDevYT@users.noreply.github.com>
2026-09-06 23:27:42 -0400

    Add: count-connected-components - submission-0

---
+++ Data Structures & Algorithms/count-connected-components/submission-0.py
@@ -0,0 +1,51 @@
+"""
+for i in 0..n-1:
+    bfs
+    visited = {0, 1, 2}
+    increment our total island count
+
+    visited = {0,1,2}
+    bfs
+    visited = {0, 1, 2}
+"""
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        #store the count of islands
+        islands = 0
+
+        #generate adjacency list
+        adj = [[] for i in range(n)]
+
+        for u,v in edges:
+            adj[v].append(u)
+            adj[u].append(v)
+        
+        #create a set of nodes we visited
+        visited = set()
+
+        def dfs(curr_node, parent):
+            to_visit = [(curr_node, parent)]
+
+            #utilize data structure behavior to not stop iterating until we've fully traversed graph
+            while to_visit:
+                curr, curr_parent = to_visit.pop()
+
+                if curr in visited: continue
+
+                #remember to add the current node to visited
+                visited.add(curr)
+
+                for v in adj[curr]:
+                    if v == parent: continue
+                    if v in visited: continue #skip adding nodes we've already seen
+
+                    to_visit.append((v, curr))
+        
+        for u in range(n):
+            if u not in visited:
+                dfs(u, -1)
+                islands += 1
+        
+        return islands
+
+        
